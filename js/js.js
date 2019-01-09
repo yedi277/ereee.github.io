09fcbb7053a774537d440c73cb24f5ae49e74669
yedi277 <yedi277@live.com>
2019-01-09 18:13:12 +0800

    1.03

---
+++ js/js.js
@@ -0,0 +1,24 @@
+function isHidden(oDiv){var vDiv = document.getElementById(oDiv);vDiv.style.display = (vDiv.style.display == 'none')?'block':'none';}
+<!-- var mobileAgent = new Array("iphone", "ipod", "ipad", "android", "mobile", "blackberry", "webos", "incognito", "webmate", "bada", "nokia", "lg", "ucweb", "skyfire");
+var browser = navigator.userAgent.toLowerCase();
+var isMobile = false;
+for (var i = 0; i < mobileAgent.length; i++) {
+	if (browser.indexOf(mobileAgent[i]) != -1) {
+	isMobile = true;
+	location.href = 'http://ereee.cc/index.htm';
+	break;}}//识别标签跳转 -->
+	
+function t1() { document.getElementById('a1').style.display = "block"; document.getElementById('a2').style.display = "none"; document.getElementById('a3').style.display = "none"; }
+function t2() { document.getElementById('a1').style.display = "none"; document.getElementById('a2').style.display = "block"; document.getElementById('a3').style.display = "none"; }
+function t3() { document.getElementById('a1').style.display = "none"; document.getElementById('a2').style.display = "none"; document.getElementById('a3').style.display = "block"; }
+function show(id){
+	var ul = document.getElementById(id);
+	if(ul.style.display == "block"){
+		ul.style.display = "none";
+	}else{
+		ul.style.display = "block"; 
+	}
+}
+
+
+
