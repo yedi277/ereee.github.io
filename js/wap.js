5e3290032b5dc8409b2f2aa2bb5531794b729854
yedi277 <yedi277@live.com>
2019-02-06 02:52:56 +0800

    Update wap.js

--- js/wap.js
+++ js/wap.js
@@ -1,3 +1,11 @@
+// var mobileAgent = new Array("iphone", "ipod", "android", "mobile", "blackberry", "webos", "incognito", "webmate", "bada", "nokia", "lg", "ucweb", "skyfire");
+// var browser = navigator.userAgent.toLowerCase();
+// var isMobile = false;
+// for (var i = 0; i < mobileAgent.length; i++) {
+	// if (browser.indexOf(mobileAgent[i]) != -1) {
+	// isMobile = true;
+	// location.href = 'http://ereee.cc/';
+	// break;}}//识别手机标签跳转 true false
 (function (win, doc) {
 if (!win.addEventListener) return;
 var html = document.documentElement;
