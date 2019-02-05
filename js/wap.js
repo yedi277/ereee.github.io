76347eb9af50554773ced7cf56c8d4050ac0d4b4
yedi277 <yedi277@live.com>
2019-02-06 01:11:17 +0800

    Create wap.js

---
+++ js/wap.js
@@ -0,0 +1,16 @@
+!function(){
+	if(/iphone|ios|android|ipod/i.test(navigator.userAgent.toLowerCase()) == false){
+		location.href = 'http://ereee.cc/index.htm';
+	}
+function isHidden(oDiv){var vDiv = document.getElementById(oDiv);vDiv.style.display = (vDiv.style.display == 'none')?'block':'none';}
+function t1() { document.getElementById('a1').style.display = "block"; document.getElementById('a2').style.display = "none"; document.getElementById('a3').style.display = "none"; }
+function t2() { document.getElementById('a1').style.display = "none"; document.getElementById('a2').style.display = "block"; document.getElementById('a3').style.display = "none"; }
+function t3() { document.getElementById('a1').style.display = "none"; document.getElementById('a2').style.display = "none"; document.getElementById('a3').style.display = "block"; }
+function show(id){
+	var ul = document.getElementById(id);
+	if(ul.style.display == "block"){
+		ul.style.display = "none";
+	}else{
+		ul.style.display = "block"; 
+	}
+}
