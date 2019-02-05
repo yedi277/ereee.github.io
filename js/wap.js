4a62d9077bda34287879df3ad9dc74f00ebb6381
yedi277 <yedi277@live.com>
2019-02-06 02:44:46 +0800

    Update wap.js

--- js/wap.js
+++ js/wap.js
@@ -1,25 +1,22 @@
-function(n) {
-    var e = n.document,
-    t = e.documentElement,
-    i = 640,
-    d = i / 100,
-    o = "orientationchange" in n ? "orientationchange": "resize",
-    a = function() {
-        var n = t.clientWidth || 320;
-        n > 640 && (n = 640),
-        t.style.fontSize = n / d + "px"
-    };
-    e.addEventListener && (n.addEventListener(o, a, !1), e.addEventListener("DOMContentLoaded", a, !1))
-} (window);
-function isHidden(oDiv){var vDiv = document.getElementById(oDiv);vDiv.style.display = (vDiv.style.display == 'none')?'block':'none';}
-function t1() { document.getElementById('a1').style.display = "block"; document.getElementById('a2').style.display = "none"; document.getElementById('a3').style.display = "none"; }
-function t2() { document.getElementById('a1').style.display = "none"; document.getElementById('a2').style.display = "block"; document.getElementById('a3').style.display = "none"; }
-function t3() { document.getElementById('a1').style.display = "none"; document.getElementById('a2').style.display = "none"; document.getElementById('a3').style.display = "block"; }
-function show(id){
-	var ul = document.getElementById(id);
-	if(ul.style.display == "block"){
-		ul.style.display = "none";
-	}else{
-		ul.style.display = "block"; 
-	}
+(function (win, doc) {
+if (!win.addEventListener) return;
+var html = document.documentElement;
+function setFont() {
+    var html = document.documentElement;
+    var cliWidth = html.clientWidth;
+    var k = 640;
+    if (cliWidth >= 640){
+      cliWidth = 640;
+    }
+    html.style.fontSize = cliWidth / k * 100 + "px";
 }
+setFont();
+setTimeout(function () {
+    setFont();
+}, 300);
+doc.addEventListener('DOMContentLoaded', setFont, false);
+win.addEventListener('resize', setFont, false);
+win.addEventListener('load', setFont, false);
+})(window, document);
+
+function isHidden(oDiv){var vDiv = document.getElementById(oDiv);vDiv.style.display = (vDiv.style.display == 'none')?'block':'none';}
